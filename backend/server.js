3c9e996bdfd1c2a06b4ab9523af618ca0ccfd5a1
Ksonalidora <ksonalidora02@gmail.com>
2024-07-26 16:59:21 +0530

    1st part completed - backend

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import path from "path";
+import express from "express";
+
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+// import { getUsersForSidebar } from "../controllers/user.controller.js";
+
+const app=express();
+const PORT=process.env.PORT || 5000;
+
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth",authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+
+
+
+app.listen(PORT,() => {
+
+    connectToMongoDB();
+    console.log(`server running on port ${PORT}`);
+    });
