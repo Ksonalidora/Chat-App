b03436862561453fc70f72c4fd66f20d2c0073f3
Ksonalidora <ksonalidora02@gmail.com>
2024-07-27 16:11:53 +0530

    2nd part completed-ui design

---
+++ frontend/src/pages/signup/GenderCheckBox.jsx
@@ -0,0 +1,36 @@
+
+
+const GenderCheckBox = () => {
+  return (
+    <div className='flex'>
+        <div className='form-control'>
+        <label className={`label gap-2 cursor-pointer`}>
+					<span className='label-text'>Male</span>
+					<input
+						type='checkbox'
+						className='checkbox border-slate-900'
+						// checked={selectedGender === "male"}
+						// onChange={() => onCheckboxChange("male")}
+					/>
+				</label>
+
+        </div>
+        <div className='form-control'>
+        <label className={`label gap-2 cursor-pointer`}>
+					<span className='label-text'>Female</span>
+					<input
+						type='checkbox'
+						className='checkbox border-slate-900'
+						// checked={selectedGender === "female"}
+						// onChange={() => onCheckboxChange("female")}
+					/>
+				</label>
+
+        </div>
+        
+
+    </div>
+  );
+};
+
+export default GenderCheckBox
